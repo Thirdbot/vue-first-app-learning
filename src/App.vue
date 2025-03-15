3e522ec35b4bac1107a85063374814c4cbb23769
Third <astroll656@gmail.com>
2025-03-15 23:40:19 +0700

    learn export defaul App.vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            friends:[
+                {
+                    id:'manuel',
+                    name:'Manuel Lorenz',
+                    phone:'01234567890',
+                    email:'manuel@localhost.com'
+                },
+                {
+                    id:'julie',
+                    name:'Julie Jones',
+                    phone:'01234567890',
+                    email:'julie@localhost.com'
+                }
+            ]
+        }
+    }
+}
+</script>
