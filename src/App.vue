bae8e1b234a4f96a20c4b670d02f4b407480d056
Third <astroll656@gmail.com>
2025-03-16 14:50:36 +0700

    form component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <section>
         <header>My friends</header>
+        <friend-add @add-contact="AddContact"></friend-add>
         <ul>
             <friend-contact
             v-for="friend in friends"
@@ -46,6 +47,16 @@ export default {
           (friend) => friend.id === id
         );
         friendIdentify.fav = !friendIdentify.fav;
+      },
+      AddContact(name,email,phone){
+        const newFriendContact = {
+          id:new Date().toISOString(),
+          name:name,
+          phone:phone,
+          email:email,
+          fav:false
+        }
+        this.friends.push(newFriendContact);
       }
     }
 }
@@ -83,7 +94,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -115,5 +127,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+        <form @submit.prevent="SubmitData">
+            <div>
+                <label for="name">name:</label>
+                <input type="text" v-model="enteredName
+                "><br>
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="text" v-model="enteredEmail
+                "><br>
+            </div>
+            <div>
+                <label for="phone">Phone:</label>
+                <input type="text" v-model="enteredPhone
+                "><br>
+            </div>
+            
+            <button>Add Contact</button>
+
+        </form>
+</template>
+
+<script>
+export default {
+    emits:['add-contact'],
+    data(){
+        return {
+            enteredName:'',
+            enteredPhone:'',
+            enteredEmail:''
+        }
+    },
+    methods:{
+        SubmitData(){
+            this.$emit('add-contact',
+                this.enteredName,this.enteredEmail,this.enteredPhone
+            )
+        }
+    }
+}
+</script>
