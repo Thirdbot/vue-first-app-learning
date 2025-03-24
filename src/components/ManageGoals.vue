a9ebc46ace0522d57ce257f1d7867fd7c9547cff
Third <astroll656@gmail.com>
2025-03-24 14:40:32 +0700

    error popup have a button now.

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,10 +3,12 @@
         <h2>Manage Goals</h2>
         <input type="text" ref="goal">
         <button @click="setGoal">Set Goal</button>
-        <error-alert v-if="inputIsInvalid">
-            <h2>please input data in form</h2>
-            <button @click="confirmError">Okay...</button>
-        </error-alert>
+        <teleport to="body">
+            <error-alert v-if="inputIsInvalid">
+                <h2>please input data in form</h2>
+                <button @click="confirmError">Okay...</button>
+            </error-alert>
+        </teleport>
     </div>
 
 </template>
